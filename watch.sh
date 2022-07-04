e61b09878fa9094da4f240d584a12aa639d0c1ee
Shakirul Hasan <shakirulhkhan@gmail.com>
2022-07-05 00:05:14 +0600

    written: Letter of transmittal

--- watch.sh
+++ watch.sh
@@ -1,1 +1,1 @@
-watchexec -r -e tex,jpg,jpeg,png,bib -- "texi2pdf -q main.tex && rm -f **/*.aux *.aux *.bbl *.blg *.fdb_latexmk *.fls *.log *.out *.toc"
+watchexec -r -e tex,jpg,jpeg,png,bib -- "xelatex -q main.tex && rm -f **/*.aux *.aux *.bbl *.blg *.fdb_latexmk *.fls *.log *.out *.toc"
